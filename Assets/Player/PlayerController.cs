f601ccb36d72a01dbeddbd4227ea7e86c4ad347c
Szilvia Bogadi <szilvia.bogadi@student.upt.ro>
2026-05-07 15:30:20 +0200

    Added model and better controller

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,16 +1,68 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] TextMeshProUGUI hud;
+    public float throttleIncrement = 0.1f;
+    public float maxThrust = 200f;
+    public float responsiveness = 10f;
+    public float lift = 135f;
+
+    private float throttle;
+    private float roll;
+    private float pitch;
+    private float yaw;
+
+    Rigidbody rb;
+
+    private float responseModifier
     {
-        
+        get
+        {
+            return (rb.mass / 10f) * responsiveness;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void HandleInputs()
+    {
+        roll = Input.GetAxis("Roll");
+        pitch = Input.GetAxis("Pitch");
+        yaw = Input.GetAxis("Yaw");
+
+        if (Input.GetKey(KeyCode.Space)) throttle += throttleIncrement;
+        else if (Input.GetKey(KeyCode.LeftControl)) throttle -= throttleIncrement;
+
+        throttle = Mathf.Clamp(throttle, 0f, 100f);
+    }
+
+    private void Update()
+    {
+        HandleInputs();
+        UpdateHud();
+    }
+
+    private void FixedUpdate()
+    {
+        // throttle = percentage
+        rb.AddForce(maxThrust * throttle * transform.forward);
+        // rotation
+        rb.AddTorque(responseModifier * yaw * transform.up);
+        rb.AddTorque(pitch * responseModifier * transform.right);
+        rb.AddTorque(responseModifier * roll * -transform.forward);
+        rb.AddForce(lift * rb.linearVelocity.magnitude * Vector3.up);
+    }
+
+    private void UpdateHud()
+    {
+        hud.text = $"Throttle: {throttle:F0} %{Environment.NewLine}" +
+            $"Airspeed: {(rb.linearVelocity.magnitude * 3.6f).ToString("F0")} km/h{Environment.NewLine}" +
+            $"Altitude: {transform.position.y:F0} m";
     }
 }
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform[] povs;
+    [SerializeField] float speed;
+
+    private int index = 1;
+    private Vector3 target;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) index = 0;
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) index = 1;
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) index = 2;
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) index = 3;
+
+        target = povs[index].position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        transform.forward = povs[index].forward;
+    }
+}
