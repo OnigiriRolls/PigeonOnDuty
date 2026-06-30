3e3744285b0fd5babe177ab281f882e3261744f3
Szilvia Bogadi <szilvia.bogadi@student.upt.ro>
2026-06-30 16:48:40 +0300

    Started human delivery

---
+++ Assets/Scripts/HumanDeliveryMission/PedestrianMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PedestrianMovement : MonoBehaviour
+{
+    public float CurrentSpeed => agent.velocity.magnitude;
+
+    public bool HasReachedDestination
+    {
+        get
+        {
+            if (agent.pathPending)
+                return false;
+            return agent.remainingDistance <= agent.stoppingDistance;
+        }
+    }
+
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void MoveTo(Vector3 destination)
+    {
+        agent.isStopped = false;
+        agent.SetDestination(destination);
+    }
+
+    public void Stop()
+    {
+        agent.isStopped = true;
+    }
+
+    public void Resume()
+    {
+        agent.isStopped = false;
+    }
+}
